575c9e80aae17997a0daec8bbd4651b1b7460705
dfh <dfanghu@gmail.com>
2018-01-16 23:33:19 +1100

    mnist basic exercise

---
+++ mnist.py
@@ -0,0 +1,187 @@
+import pickle as pkl
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data')
+data_dir = './data'
+import os
+from glob import glob
+import helper
+helper.download_extract('mnist', data_dir)
+helper.download_extract('celeba', data_dir)
+mnist_dataset = helper.Dataset('mnist', glob(os.path.join(data_dir, 'mnist/*.jpg')))
+N,h,w,d = mnist_dataset.shape
+
+# Size of input image to discriminator
+input_size = 784
+# Size of latent vector to generator
+z_dim = 128
+# Sizes of hidden layers in generator and discriminator
+g_hidden_size = 128
+d_hidden_size = 128
+# Leak factor for leaky ReLU
+alpha = 0.01
+n_units = 128
+# Smoothing
+smooth = 0.05
+
+def model_inputs(real_dim, z_dim):
+    inputs_real = tf.placeholder(tf.float32, (None, real_dim), name='input_real')
+    inputs_z = tf.placeholder(tf.float32, (None, z_dim), name='input_z')
+    return inputs_real, inputs_z
+
+
+def generator(z, out_channel_dim, is_train=True):
+    with tf.variable_scope('generator', reuse=(not is_train)):
+        h1 = tf.layers.dense(z, n_units, activation=None)
+        h1 = tf.maximum(alpha * h1, h1)
+        logits = tf.layers.dense(h1, out_channel_dim, activation=None)
+        out = tf.tanh(logits)
+    return out
+
+
+def discriminator(images, is_train=True):
+    with tf.variable_scope('discriminator', reuse=(not is_train)):
+        h1 = tf.layers.dense(images, n_units, activation=None)
+        h1 = tf.maximum(alpha * h1, h1)
+        logits = tf.layers.dense(h1, 1, activation=None)
+        out = tf.sigmoid(logits)
+    return out, logits
+
+
+
+
+def model_loss(input_real, input_z, out_channel_dim):
+    # Build the model
+    g_model = generator(input_z, out_channel_dim)
+    # g_model is the generator output
+
+    d_model_real, d_logits_real = discriminator(input_real)
+    d_model_fake, d_logits_fake = discriminator(g_model, is_train=False)
+
+
+    # Calculate losses
+    d_loss_real = tf.reduce_mean(
+                      tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real,
+                                                              labels=tf.ones_like(d_logits_real) - smooth))
+    d_loss_fake = tf.reduce_mean(
+                      tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake,
+                                                              labels=tf.zeros_like(d_logits_real) + smooth))
+    d_loss = d_loss_real + d_loss_fake
+
+    g_loss = tf.reduce_mean(
+                 tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake,
+                                                         labels=tf.ones_like(d_logits_fake)))
+    return d_loss, g_loss
+
+
+# Optimizers
+learning_rate = 0.002
+def model_opt(d_loss, g_loss, learning_rate, beta1):
+    """
+    Get optimization operations
+    :param d_loss: Discriminator loss Tensor
+    :param g_loss: Generator loss Tensor
+    :param learning_rate: Learning Rate Placeholder
+    :param beta1: The exponential decay rate for the 1st moment in the optimizer
+    :return: A tuple of (discriminator training operation, generator training operation)
+    """
+    t_vars = tf.trainable_variables()
+    g_vars = [var for var in t_vars if var.name.startswith('generator')]
+    d_vars = [var for var in t_vars if var.name.startswith('discriminator')]
+
+    d_train_opt = tf.train.AdamOptimizer(learning_rate, beta1).minimize(d_loss, var_list=d_vars)
+    g_train_opt = tf.train.AdamOptimizer(learning_rate, beta1).minimize(g_loss, var_list=g_vars)
+    return d_train_opt, g_train_opt
+
+
+batch_size = 1000
+epochs = 20
+
+
+def view_sample(sample):
+    fig, axes = plt.subplots(figsize=(7, 7), nrows=4, ncols=4, sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), sample):
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        im = ax.imshow(img.reshape((28, 28)), cmap='Greys_r')
+
+    return fig, axes
+
+def show_generator_output(sess, n_images, input_z, out_channel_dim, image_mode):
+    """
+    Show example output for the generator
+    :param sess: TensorFlow session
+    :param n_images: Number of Images to display
+    :param input_z: Input Z Tensor
+    :param out_channel_dim: The number of channels in the output image
+    :param image_mode: The mode to use for images ("RGB" or "L")
+    """
+    cmap = None if image_mode == 'RGB' else 'gray'
+    z_dim = input_z.get_shape().as_list()[-1]
+    example_z = np.random.uniform(-1, 1, size=[n_images, z_dim])
+    samples = sess.run(
+        generator(input_z, out_channel_dim, False),
+        feed_dict={input_z: example_z})
+
+    images_grid = helper.images_square_grid(samples.reshape((n_images, 28,28,-1)), image_mode)
+    plt.imshow(images_grid, cmap=cmap)
+    plt.show()
+
+def train(epoch_count, batch_size, z_dim, learning_rate, beta1,
+          get_batches, data_shape, data_image_mode):
+    """
+    Train the GAN
+    :param epoch_count: Number of epochs
+    :param batch_size: Batch Size
+    :param z_dim: Z dimension
+    :param learning_rate: Learning Rate
+    :param beta1: The exponential decay rate for the 1st moment in the optimizer
+    :param get_batches: Function to get batches
+    :param data_shape: Shape of the data
+    :param data_image_mode: The image mode to use for images ("RGB" or "L")
+    """
+    _, image_width, image_height, image_channels = data_shape
+    out_channel_dim = image_width * image_height * image_channels
+    tf.reset_default_graph()
+
+    input_real, input_z = model_inputs(input_size, z_dim)
+    d_loss, g_loss = model_loss(input_real, input_z, out_channel_dim)
+    d_train_opt, g_train_opt = model_opt(d_loss, g_loss, learning_rate, beta1)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for e in range(epoch_count):
+
+            # for ii in range(mnist.train.num_examples // batch_size):
+            #     batch = mnist.train.next_batch(batch_size)
+            #     batch_images = batch[0].reshape((batch_size, 784))
+            #     batch_images = batch_images * 2 - 1
+
+            for ii in range(N // batch_size):
+                batches = get_batches(batch_size)
+                batch_images = next(batches)
+                batch_images = batch_images.reshape((batch_size, out_channel_dim)) * 2
+
+                batch_z = np.random.uniform(-1, 1, size=(batch_size, z_dim))
+
+                # Run optimizers
+                _ = sess.run(d_train_opt, feed_dict={input_real: batch_images, input_z: batch_z})
+                _ = sess.run(g_train_opt, feed_dict={input_z: batch_z})
+
+            # At the end of each epoch, get the losses and print them out
+            train_loss_d = sess.run(d_loss, {input_z: batch_z, input_real: batch_images})
+            train_loss_g = g_loss.eval({input_z: batch_z})
+
+            print("Epoch {}/{}...".format(e + 1, epochs),
+                  "Discriminator Loss: {:.4f}...".format(train_loss_d),
+                  "Generator Loss: {:.4f}".format(train_loss_g))
+            # Save losses to view after training
+            losses.append((train_loss_d, train_loss_g))
+            show_generator_output(sess, 1, input_z, out_channel_dim, data_image_mode)
+
+
+train(epochs,batch_size,z_dim,learning_rate,0.9,mnist_dataset.get_batches, mnist_dataset.shape,
+      mnist_dataset.image_mode)
+
